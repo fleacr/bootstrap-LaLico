d2f477c7458590407b024ee1b52b5bb06da3c68b
Alejandro Carvajal <ale.carva16@hotmail.com>
2023-08-25 10:15:47 -0600

    BackGround added, navBar changes

---
+++ javascript.js
@@ -0,0 +1,5 @@
+// Escuchador de evento para cargar la imagen
+document.addEventListener('DOMContentLoaded', function() {
+    const image = document.querySelector('.opacity-transition');
+    image.classList.add('loaded'); // Agrega la clase "loaded" para cambiar la opacidad
+  });
