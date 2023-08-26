ab8c3f2b943207f798438ed4da41e89992722396
Alejandro Carvajal <ale.carva16@hotmail.com>
2023-08-26 09:25:15 -0600

    Location Added

--- javascript.js
+++ javascript.js
@@ -3,3 +3,13 @@ document.addEventListener('DOMContentLoaded', function() {
     const image = document.querySelector('.opacity-transition');
     image.classList.add('loaded'); // Agrega la clase "loaded" para cambiar la opacidad
   });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    const image = document.querySelector('.imperial-transition-right');
+    image.classList.add('loaded'); // Agrega la clase "loaded" para cambiar la opacidad
+  });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    const image = document.querySelector('.imperial-transition-left');
+    image.classList.add('loaded'); // Agrega la clase "loaded" para cambiar la opacidad
+  });
