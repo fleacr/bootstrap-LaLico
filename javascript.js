06b6591144dfa9209729fcb4ffee5113c6186858
Alejandro Carvajal <ale.carva16@hotmail.com>
2023-08-29 20:27:11 -0600

    Frames for products added, issue in animations

--- javascript.js
+++ javascript.js
@@ -1,3 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const image = document.querySelector('.imperial-transition-left');
+  image.classList.add('loaded'); // Agrega la clase "loaded" para cambiar la opacidad
+});
+
 // Escuchador de evento para cargar la imagen
 document.addEventListener('DOMContentLoaded', function() {
     const image = document.querySelector('.opacity-transition');
@@ -6,10 +11,5 @@ document.addEventListener('DOMContentLoaded', function() {
 
   document.addEventListener('DOMContentLoaded', function() {
     const image = document.querySelector('.imperial-transition-right');
-    image.classList.add('loaded'); // Agrega la clase "loaded" para cambiar la opacidad
-  });
-
-  document.addEventListener('DOMContentLoaded', function() {
-    const image = document.querySelector('.imperial-transition-left');
     image.classList.add('loaded'); // Agrega la clase "loaded" para cambiar la opacidad
   });
